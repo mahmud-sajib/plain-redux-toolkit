a6fc4916266215e2149aae43cbbccb7467d0df78
Mahmud Sajib <shout.mahmud@gmail.com>
2023-06-12 00:24:27 +0600

    applying middleware

--- rtk/app/store.js
+++ rtk/app/store.js
@@ -1,12 +1,21 @@
-const configureStore = require("@reduxjs/toolkit").configureStore;
+const { configureStore } = require("@reduxjs/toolkit");
 
+// importing slices
 const counterReducer = require("../features/counter/counterSlice");
+const keeperReducer = require("../features/keeper/keeperSlice");
+
+// importing logger from redux-logger middleware
+const { default: logger } = require("redux-logger");
 
 // configuring store (providing the reducer to redux store)
 const store = configureStore({
   reducer: {
     counter: counterReducer, // Assigning the counterReducer to the "counter" slice of the store
+    keeper: keeperReducer, // Assigning the keeperReducer to the "keeper" slice of the store
   },
+  // getDefaultMiddlewares() is redux's own middleware.
+  // We need to return it first & concat our own middleware to use it
+  middleware: (getDefaultMiddlewares) => getDefaultMiddlewares().concat(logger),
 });
 
 module.exports = store;
--- rtk/index.js
+++ rtk/index.js
@@ -1,17 +1,23 @@
 const store = require("./app/store");
 const { counterActions } = require("./features/counter/counterSlice");
+const { keeperActions } = require("./features/keeper/keeperSlice");
 
 // initial state
-console.log(store.getState());
+// console.log(store.getState());
 
 // subscribe to state changes
 store.subscribe(() => {
-  console.log(store.getState());
+  //   console.log(store.getState());
 });
 
-// disptach actions
+// disptach counter actions
 store.dispatch(counterActions.increment());
 
 store.dispatch(counterActions.increment());
 
 store.dispatch(counterActions.decrement());
+
+// disptach keeper actions
+store.dispatch(keeperActions.increment(10));
+
+store.dispatch(keeperActions.decrement(5));
